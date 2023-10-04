331d33c25aef9b50207da63d5a4c4a3c396f64cd
Svilen Dilchev <svilen.dilchev@kcl.ac.uk>
2023-10-04 21:14:44 +0300

    Add frequency counter to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,27 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
+    newList = transformAllItemsToStrings(items)
+    uniqueItems = []
     frequencies = {}
-    # Your code goes here
+    for item in newList:
+        print(f"pure item - {item}, string version - {str(item)}")
+        if uniqueItems.count(item) == 0 & uniqueItems.count(str(item)) == 0:
+            uniqueItems.append(item)
+
+    for item in uniqueItems:
+        frequencies[item] = newList.count(item)
     return frequencies
+
+
+def transformAllItemsToStrings(items):
+    newList = []
+    for item in items:
+        if type(item) == int:
+            newList.append(f'{item}')
+        else:
+            newList.append(item)
+    return newList
+
